c31d08fb396eff285498d05befc8bf2c64d30f86
administrator <liquidnuker@users.noreply.github.com>
2017-07-29 10:20:24 -0700

    testroutes ok

--- src/index.jsx
+++ src/index.jsx
@@ -1,87 +1,102 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//   </div>
-// )
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 function Home(props) {
   return (
     <div>
-    <h2>Home</h2>
+    <h2>Home_</h2>
   </div>
   );
 }
 
 function About(props) {
   return (
-    <div>
-    <h2>About</h2>
-  </div>
+    // ok
+    <div>     
+      <Link to="/">Home</Link>             
+    </div>  
   );
 }
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
+function Testsub({match}) {
+  return (
+    <div>
+      <p>match.params.spec: {match.params.spec}</p>
+      <p>match.url: {match.url}</p>      
+    </div>
+  );
+}
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
+function Test({match}) {
+  return (
+    <div>
 
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
+      <Link to={`${match.url}/unsorted`}>
+          unsorted
+      </Link>
 
-function BasicExample(props) {
+      <Route exact path={`${match.url}/:spec`} component={Testsub}/>
+
+      <p>test</p>
+      match.params.id: {match.params.id}
+      <p>match.url: {match.url}</p>
+    </div>
+  );
+}
+
+function Contact({match}) {
+  let x = match.params.id
+  return (
+    <div>
+      <p>match.params.id: {x}</p>
+      <p>match.url: {match.url}</p>
+    </div>
+  );
+}
+
+function ContactMain({match}) {
+  return (
+    // ok
+    <div>     
+      <p>contact</p>
+
+      <Link to={`${match.url}/person-1`}>
+          person1
+      </Link>
+
+      <Route exact path={`${match.url}/:id`} component={Contact}/>
+
+    {/* optional */}
+      {/*<Route exact path={match.url} render={() => (
+      <h3>Contact main</h3>
+      )}/>*/}
+    </div>  
+  );
+}
+
+function RouterMain(props) {
   return (
     <Router>
     <div>
       <ul>
-        <li><Link to="#/">Home</Link></li>
-        <li><Link to="#/about">About</Link></li>
-        <li><Link to="#/topics">Topics</Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/test/link1">test</Link></li>
       </ul>
 
       <hr/>
 
-      <Route exact path="#/" component={Home}/>
-      <Route path="#/about" component={About}/>
-      <Route path="#/topics" component={Topics}/>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={ContactMain}/>
+      <Route path="/test/link1" component={Test}/>
+      
     </div>
   </Router>
   );
 }
 
-
 ReactDOM.render(
-  <BasicExample />,
+  <RouterMain />,
   document.getElementById('root')
 );
